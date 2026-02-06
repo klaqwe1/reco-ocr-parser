8f1ffa80d8834e19c37914b823dcec763493dd96
klaqwe1 <klaqwe1@naver.com>
2026-02-07 00:01:50 +0900

    first commit

---
+++ src/main/java/me/eunseong/ocrtextparser/OcrTextParserApplication.java
@@ -0,0 +1,13 @@
+package me.eunseong.ocrtextparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrTextParserApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OcrTextParserApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/eunseong/ocrtextparser/OcrTextParserApplicationTests.java
@@ -0,0 +1,13 @@
+package me.eunseong.ocrtextparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrTextParserApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
